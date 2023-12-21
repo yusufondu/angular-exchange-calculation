d9835a1444883fd820641ee21ea35153593e6487
Yusuf Öndü <yusuf.ondu@pusulayazilim.com.tr>
2023-12-21 13:06:38 +0300

    refactor: removed code duplication with @for

--- src/app/shared/components/molecules/header/header.component.ts
+++ src/app/shared/components/molecules/header/header.component.ts
@@ -3,6 +3,11 @@ import { ImageComponent } from '../../atoms/ee-image/ee-image.component';
 import { TextComponent } from '../../atoms/ee-text/ee-text.component';
 import { ButtonComponent } from '../../atoms/ee-button/ee-button.component';
 
+interface HeaderTexts {
+  class: string;
+  text: string;
+}
+
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -10,4 +15,21 @@ import { ButtonComponent } from '../../atoms/ee-button/ee-button.component';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  headerTexts: HeaderTexts[] = [
+    {
+      class: 'header-title-black',
+      text: 'Exchange Your Money',
+    },
+    {
+      class: 'header-title-green',
+      text: 'Easily, Quickly And Securely',
+    },
+    {
+      class: 'header-description',
+      text: `Best source for currency conversion, sending money online and tracking
+      exchange rates. Live tracking and notifications + flexible delivery and
+      payment options.`,
+    },
+  ];
+}
